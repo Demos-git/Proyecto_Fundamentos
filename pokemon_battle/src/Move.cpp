ca51c051a413c639e7ecc947eae68e73fa05bb99
Demos-git <00111026@uca.edu.sv>
2026-06-25 12:17:30 -0600

    Delete pokemon_battle directory

--- pokemon_battle/src/Move.cpp
+++
@@ -1,7 +0,0 @@
-#include "Move.h"
-
-using namespace std;
-
-Move::Move(string n, Type t, MoveCategory cat, int pow, int acc, int pp_)
-    : name(move(n)), type(t), category(cat),
-      power(pow), accuracy(acc), pp(pp_), currentPP(pp_) {}
